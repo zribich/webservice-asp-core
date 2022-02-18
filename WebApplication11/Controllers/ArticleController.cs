69af566fd10c30223f6be89a022c5979182d3d22
unknown <zribi.omar3@gmail.com>
2022-02-18 21:20:13 +0100

    finish

--- WebApplication11/Controllers/ArticleController.cs
+++ WebApplication11/Controllers/ArticleController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,14 @@ namespace WebApplication11.Controllers
         {
             _context = context;
         }
+        //GET:api/Articles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Article>>> GetArticles()
+        {
+            return await _context.Article.ToListAsync();
 
+        }
+        //GET:api/Articles/0001
         [HttpGet("{id}")]
         public async Task<ActionResult<Article>> GetArticles(string id)
         {
@@ -28,6 +36,55 @@ namespace WebApplication11.Controllers
                 return NotFound();
             }
             return Article;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Article>> PostArticles( Article article)
+        {
+            _context.Article.Add(article);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetArticles) , new {codart=article.codart }, article);
+
+        }
+        [HttpPut("{id}") ]
+        public async Task<IActionResult> PutArticles(string id, Article article)
+        {
+            if (id != article.codart )
+            {
+                return BadRequest();
+            }
+            _context.Entry(article).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException )
+            {  if(!articlesExists(id) )
+                {
+                    return NotFound();
+                }
+            else
+                {
+                    throw;
+                }
+                
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArticles(string id)
+        {
+            var article = await _context.Article.FindAsync(id);
+            if(article == null)
+            {
+                return NotFound();
+            }
+            _context.Article.Remove(article);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        private bool articlesExists(string id)
+        {
+            return _context.Article.Any(e => e.codart == id);
         }
     }
 }
